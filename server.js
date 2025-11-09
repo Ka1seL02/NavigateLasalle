b9272a0cb8fc7fed335c8f1f674a6a8c431272ce
Ka1seL02 <klydel0219@gmail.com>
2025-11-09 17:53:51 +0800

    Added all relevant API endpoints & Routes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const connectDB = require('./db');
-
-const faqRoutes = require('./routes/api/faqs');
+// Import route modules for handling API endpoints
 const accountRoutes = require('./routes/api/accounts');
 const feedbackRoutes = require('./routes/api/feedbacks');
+const newsRoutes = require('./routes/api/news');
+const facilitiesRoutes = require('./routes/api/facilities');
+const faqRoutes = require('./routes/api/faqs');
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.use(express.urlencoded({ extended: true })); // For parsing HTML Data
 // Routes
 app.use('/api/accounts', accountRoutes);
 app.use('/api/feedbacks', feedbackRoutes);
+app.use('/api/news', newsRoutes);
+app.use('/api/facilities', facilitiesRoutes);
 app.use('/api/faqs', faqRoutes);
 
 // Optional: serve static frontend
