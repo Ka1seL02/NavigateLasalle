ea57c5fb3f6f10906a5b59e233f9307e457f508e
Ka1seL02 <klydel0219@gmail.com>
2025-11-24 10:56:39 +0800

    Added Category

--- models/FAQ.js
+++ models/FAQ.js
@@ -8,6 +8,11 @@ const faqSchema = new mongoose.Schema({
     answer : {
         type: String,
         required: true
+    },
+    category : {
+        type: String,
+        required: true,
+        enum: ['General', 'News', 'Interactive Map', 'Virtual Tour', 'Feedback']
     }
 }, {
     collection: 'faqs'
