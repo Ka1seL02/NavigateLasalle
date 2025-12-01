244c6893d24489ee63798dd3611d246e8b21a6e8
Ka1seL02 <klydel0219@gmail.com>
2025-12-01 13:55:31 +0800

    Added fetching of 5 latest featured news

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const Account = require('../../models/News');
+const News = require('../models/News');
+
+// GET latest 5 featured news
+router.get('/featured', async (req, res) => {
+  try {
+    const featuredNews = await News.find({ status: 'posted' })
+      .sort({ datePosted: -1 }) 
+      .limit(5);
+
+    res.json(featuredNews);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
